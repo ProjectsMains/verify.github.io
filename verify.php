c8f2b660cdfdf79435d89af4d5666b00f0b4f37f
ProjectsMains <120784704+ProjectsMains@users.noreply.github.com>
2022-12-17 17:55:39 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,16 @@
-
-   
-                 <?php 
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <style>
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+  
+<body>
+  <?php 
                   if(isset($_POST["email"]) && isset($_POST["code"])){
 $to=$_POST["email"];
 $subject=$_POST["Verification Code"];
@@ -8,5 +18,10 @@ $message="Your Verification code: " .$_POST["code"];
 mail($to,$subject,$message);
 echo "send successfully";}
             ?>
+</body>
+  
+</html>
+   
+               
     
 
